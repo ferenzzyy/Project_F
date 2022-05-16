090a06472adfe5c67ce0e56492f4fa8e85aa1adb
Ferenzzyy <66218273+Ferenzzyy@users.noreply.github.com>
2022-05-16 05:20:35 +0100

    Delete Project F directory

--- Project F/main.py
+++
@@ -1,7 +0,0 @@
-from game import Game  #Imports  the Game class from game.py
-
-g = Game() #Makes the class the into a variable for an easier use
-
-while g.running: #Main loop for the game itself by calling the variables from the Game class
-    g.curr_menu.display_menu()
-    g.game_loop()
